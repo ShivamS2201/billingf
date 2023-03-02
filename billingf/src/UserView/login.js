fe61ed4ed13550412a1603915fd6f7fb1b27e347
Shivam S <the.alpha.tronixm@gmail.com>
2023-03-03 01:11:40 +0530

    added

---
+++ billingf/src/UserView/login.js
@@ -0,0 +1,18 @@
+import Form from 'react-bootstrap/Form';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import {Navbar} from 'react-bootstrap';
+
+const LOGIN = ()=>{
+    console.log('LOGIN ENabled');
+    return(
+        <div>
+             <Navbar bg="light">
+        
+      </Navbar>
+            
+        </div>
+    )
+}
+
+export default LOGIN;
